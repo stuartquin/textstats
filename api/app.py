df5b130c767e62d66b3bd6598d055e3ec862ff56
Stuart Quin <stuart.quin@gmail.com>
2014-02-15 16:48:15 +0000

    Support query params for order and limiting

--- api/app.py
+++ api/app.py
@@ -8,16 +8,48 @@ import json
 
 corpus = Frequencies("en")
 
-def process(nodes):
+valid_keys = ["z-score", "word", "is_stop", "tf", "idf", "corpus_frequency",
+              "weighted_frequency"]
+
+def get_parameters(args):
+    params = {
+        "order_by":  args.get("order_by", "z-score"),
+        "limit": int(args.get("limit", 10)),
+        "direction": args.get("direction", "desc")
+    }
+
+    if params["order_by"] not in valid_keys:
+        raise Exception("Invalid order by")
+
+    return params
+
+def apply_params(words, params):
+    reverse = False
+    if params["direction"] == "desc":
+        reverse = True
+
+    results = sorted(words, key=lambda k: k[params["order_by"]], reverse=reverse) 
+    return results[0:min(len(results), params["limit"])]
+
+def process(request):
+    data = request.data
+    # Remove control characters
+    data = json.loads(''.join(c for c in data if ord(c) >= 32))
+    if "nodes" not in data:
+        return {}
+
+    params = get_parameters(request.args)
     stats = TextStats("en", corpus)
+
+    nodes = data["nodes"]
     for node in nodes:
         stats.add_node(node["text"], node["weighting"])
 
     stats.calculate_scores()
     words = stats.stats
-    zsorted = sorted(words.values(), key=lambda k: k["z-score"], reverse=True) 
+    results = apply_params(words.values(), params)
 
-    return {"words":zsorted, "meta": stats.meta}
+    return {"words":results, "meta": stats.meta}
 
 
 @Request.application
@@ -26,13 +58,10 @@ def application(request):
     mpa = dict.fromkeys(range(32))
 
     if request.method == "POST":
-        data = request.data
-        # Remove control characters
-        data = json.loads(''.join(c for c in data if ord(c) >= 32))
-        if "nodes" in data:
-            results = process(data["nodes"])
+        results = process(request)
 
-    return Response(json.dumps(results), headers={"Content-Type": "application/json"})
+    return Response(json.dumps(results),
+                    headers={"Content-Type": "application/json"})
 
 
 if __name__ == "__main__":
